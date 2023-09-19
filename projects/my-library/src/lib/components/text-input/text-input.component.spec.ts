af6bb110165873278992fde3d9121e63d7bc80ae
José Matos <developer@josematos.work>
2023-09-19 10:40:38 +0200

    Small fix + add unit tests base structure + first unit tests

--- projects/my-library/src/lib/components/text-input/text-input.component.spec.ts
+++ projects/my-library/src/lib/components/text-input/text-input.component.spec.ts
@@ -1,15 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { TextInputComponent } from './text-input.component';
 
 describe('TextInputComponent', () => {
   let component: TextInputComponent;
   let fixture: ComponentFixture<TextInputComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FormsModule],
       declarations: [TextInputComponent],
-    });
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(TextInputComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -17,5 +22,43 @@ describe('TextInputComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should handle input change', () => {
+    spyOn(component, 'onInputChange');
+    const inputElement = fixture.debugElement.query(
+      By.css('input')
+    ).nativeElement;
+    inputElement.value = 'test';
+    inputElement.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+
+    expect(component.onInputChange).toHaveBeenCalled();
+  });
+
+  it('should handle registerOnChange', () => {
+    const testFn = jasmine.createSpy();
+    component.registerOnChange(testFn);
+    expect(component.onChange).toBe(testFn);
+  });
+
+  it('should handle registerOnTouched', () => {
+    const testFn = jasmine.createSpy();
+    component.registerOnTouched(testFn);
+    expect(component.onTouched).toBe(testFn);
+  });
+
+  it('should set disabled state', () => {
+    component.setDisabledState(true);
+    expect(component.isDisabledState).toBeTrue();
+
+    component.setDisabledState(false);
+    expect(component.isDisabledState).toBeFalse();
+  });
+
+  it('should write value', () => {
+    const testValue = 'test value';
+    component.writeValue(testValue);
+    expect(component.value).toBe(testValue);
   });
 });
