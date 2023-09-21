582df7ddb0782d81573a91f20ab83b57eca06b38
José Matos <developer@josematos.work>
2023-09-21 11:46:33 +0200

    Isolate css from library and import it from app

--- projects/my-library/my-library.tailwind.config.js
+++ projects/my-library/my-library.tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,ts}'],
+  content: ['./projects/my-library/src/**/*.{html,ts}'],
   theme: {
     extend: {},
   },
